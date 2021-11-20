6fa39fa02ab4aa2cf412ff0f64c2be4a552c507a
carljhn <zoletacarljohn1@gmail.com>
2021-11-19 20:47:04 -1200

    Third commit: I added the if, elif, else statements as a code block for the function

--- lowestNum.py
+++ lowestNum.py
@@ -3,3 +3,9 @@ num2=int(input("Enter the second number: "))
 num3=int(input("Enter the third number: "))
 
 def get_lowestNum(num1, num2, num3):
+    if num1<num2 and num1<num3:
+        return num1
+    elif num2<num1 and num2<num3:
+        return num2
+    else:
+        return num3
